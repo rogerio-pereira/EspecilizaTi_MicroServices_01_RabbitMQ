fcd4a4d9c297606f3254cda802b1dae4680c2af1
Rogerio Pereira <hi@rogeriopereira.dev>
2021-07-20 20:38:32 -0400

    Aula: 05 - Cadastrar Nova Categoria API Laravel

---
+++ app/Observers/CategoryObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+class CategoryObserver
+{
+    /**
+     * Handle the Category "created" event.
+     *
+     * @param  \App\Models\Category  $category
+     * @return void
+     */
+    public function creating(Category $category)
+    {
+        $category->url = Str::slug($category->title, '-');
+    }
+
+    /**
+     * Handle the Category "updated" event.
+     *
+     * @param  \App\Models\Category  $category
+     * @return void
+     */
+    public function updating(Category $category)
+    {
+        $category->url = Str::slug($category->title, '-');
+    }
+}
