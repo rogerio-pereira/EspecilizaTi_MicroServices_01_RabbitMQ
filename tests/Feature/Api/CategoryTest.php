f03a04bf7bf76128fcbee9af9951ea1864fd93a7
Rogerio Pereira <hi@rogeriopereira.dev>
2021-07-21 19:45:12 -0400

    Aula: 03 - Testes no Laravel - Recuperar Categoria

--- tests/Feature/Api/CategoryTest.php
+++ tests/Feature/Api/CategoryTest.php
@@ -9,6 +9,8 @@ use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    protected $endpoint = '/categories';
+
     /**
      * Get all categories
      *
@@ -19,9 +21,37 @@ class CategoryTest extends TestCase
     {
         Category::factory()->count(6)->create();
 
-        $response = $this->getJson('/categories');
+        $response = $this->getJson($this->endpoint);
 
         $response->assertStatus(200)
             ->assertJsonCount(6, 'data');
+    }
+
+    /**
+     * Error when geting a category that doesn't exist
+     * 
+     * @test
+     */
+    public function test_error_get_single_category()
+    {
+        $fake_url = 'fake-url';
+        $response = $this->getJson($this->endpoint.'/'.$fake_url);
+
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Get single category
+     *
+     * @test
+     */
+    public function test_get_single_category()
+    {
+        $category = Category::factory()->create();
+        $url = $category->url;
+
+        $response = $this->getJson($this->endpoint.'/'.$url);
+
+        $response->assertStatus(200);
     }
 }
